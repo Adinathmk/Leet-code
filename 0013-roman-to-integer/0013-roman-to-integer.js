89e4cd80764dbe88895940f1bc5a9d897d6fd3bf
Adinathmk <168068778+Adinathmk@users.noreply.github.com>
2025-08-21 10:21:15 +0530

    Time: 2 ms (99.32%), Space: 61.2 MB (34.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,72 +3,26 @@
  * @return {number}
  */
 var romanToInt = function (s) {
-    let total=0
-    for (i = 0; i < s.length; i++) {   
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
 
-        switch (s[i]){
-            case "I":
-                if(s[i+1]=="V"||s[i+1]=="X"){
-                    break;
-                }
-                total += 1;
-                break;            
+  let total = 0;
 
-            case "V":
-                if(s[i-1]=="I"){
-                    total +=4
-                    break;
-                } 
-                total += 5;
-                break;            
-
-            case "X":
-                if(s[i-1]=="I"){
-                    total +=9
-                    break;
-                }
-                if(s[i+1]=="L"||s[i+1]=="C"){
-                    break;
-                } 
-                total += 10;
-                break;
-   
-            case "L":
-                if(s[i-1]=="X"){
-                    total +=40;
-                    break;
-                }  
-                total += 50;
-                break;
-            
-            case "C":
-                if(s[i-1]=="X"){
-                    total +=90;
-                    break;
-                }
-                if(s[i+1]=="D"||s[i+1]=="M"){
-                    break;
-                } 
-                total += 100;
-                break;
-            
-            case "D":
-                if(s[i-1]=="C"){
-                    total +=400;
-                    break;
-                }
-                total += 500;
-                break;            
-
-            case "M":
-                if(s[i-1]=="C"){
-                    total +=900;
-                    break;
-                } 
-                total += 1000;
-                break;
-            
-        }
+  for (let i = 0; i < s.length; i++) {
+    let curr = values[s[i]];
+    let next = values[s[i + 1]];
+    if (curr < next) {
+      total -= curr;
+    } else {
+      total += curr;
     }
-    return total
+  }
+  return total;
 };
