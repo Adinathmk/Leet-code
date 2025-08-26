bb6d6e06eb1604c8893ff31434d30fb14c241ef1
Adinathmk <168068778+Adinathmk@users.noreply.github.com>
2025-08-26 10:40:37 +0530

    Time: 23 ms (38.07%), Space: 73.9 MB (55.64%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let newSet=new Set(nums)
+    if(newSet.size===nums.length){
+        return false
+    }
+    else{
+        return true
+    } 
+}
+    
