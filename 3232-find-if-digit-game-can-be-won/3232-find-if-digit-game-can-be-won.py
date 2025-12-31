e6a0c9c0100184d33359d5893f230e3a9ebfa8dc
Adinathmk <168068778+Adinathmk@users.noreply.github.com>
2025-12-31 10:19:33 +0530

    Time: 0 ms (100%), Space: 17.1 MB (96.71%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.py
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.py
@@ -1,12 +1,12 @@
 class Solution:
     def canAliceWin(self, nums: List[int]) -> bool:
-        single=double=0
+        single=doublee=0
         for num in nums:
             if num//10==0:
                 single+=num
             else:
-                double+=num
-        return single!=double
+                doublee+=num
+        return single!=doublee
 
 
         
