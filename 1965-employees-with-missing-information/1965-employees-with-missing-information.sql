69f1580d3c062b51baf1eafe2027318ef2d0cd0a
Adinathmk <168068778+Adinathmk@users.noreply.github.com>
2026-03-12 09:45:20 +0530

    Time: 543 ms (61.32%), Space: 0B (100%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,7 +1,15 @@
-SELECT COALESCE(e.employee_id, s.employee_id) AS employee_id
+SELECT e.employee_id
 FROM Employees e
-FULL OUTER JOIN Salaries s
+LEFT JOIN Salaries s
+    ON e.employee_id = s.employee_id
+WHERE s.salary IS NULL
+
+UNION
+
+SELECT s.employee_id
+FROM Salaries s
+LEFT JOIN Employees e
     ON e.employee_id = s.employee_id
 WHERE e.name IS NULL
-   OR s.salary IS NULL
+
 ORDER BY employee_id;
